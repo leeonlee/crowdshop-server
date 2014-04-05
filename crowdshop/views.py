924705638b134572f7cf0bf7ba315325bf7a2d08
Leon Lee <93leonlee@gmail.com>
2014-04-05 03:09:18 -0400

    fix merge on views

--- crowdshop/views.py
+++ crowdshop/views.py
@@ -2,22 +2,18 @@ import json
 from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-<<<<<<< HEAD
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
-=======
 from rest_framework import viewsets
 from crowdshop.models import Task
 from crowdshop.serializers import UserSerializer, TaskSerializer
 from django.contrib.auth.models import User
 from rest_framework import generics
->>>>>>> models
 
 # Create your views here.
 def index(request):
 	return render_to_response("crowdshop/index.html", RequestContext(request))
 
-<<<<<<< HEAD
 def login(request):
 	results = {'success':'invalid'}
 	if request.method == 'POST':
@@ -35,7 +31,6 @@ def login(request):
 
 	response = json.dumps(results)
 	return HttpResponse(response, content_type='application/json')
-=======
 class UserViewSet(viewsets.ModelViewSet):
 	"""
 	API endpoint that allows users to be viewed or edited
@@ -55,4 +50,3 @@ class TaskList(generics.ListAPIView):
 
 	def get_queryset(self):
 		return Task.objects.filter(claimed_by=None)
->>>>>>> models
