b113fe1be29a81ff23a691d008f51f65e05cb25e
Leon Lee <93leonlee@gmail.com>
2014-04-05 00:51:13 -0400

    add task model to models.py

--- crowdshop/models.py
+++ crowdshop/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+	owner = models.ForeignKey(User)
+	title = models.CharField(max_length=255)
+	desc = models.CharField(max_length=255)
+	claimed_by = models.ForeignKey(User)
+	threshold = models.IntegerField()
