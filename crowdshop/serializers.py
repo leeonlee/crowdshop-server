954abcfffba7fc014fb378b1eb61ac36a2e50eda
Leon Lee <93leonlee@gmail.com>
2014-04-05 03:01:02 -0400

    add more fields

--- crowdshop/serializers.py
+++ crowdshop/serializers.py
@@ -5,10 +5,14 @@ from rest_framework import serializers
 class UserSerializer(serializers.HyperlinkedModelSerializer):
 	class Meta:
 		model = User
-		fields = ('url', 'username',)
+		fields = ('id', 'username',)
 
 class TaskSerializer(serializers.HyperlinkedModelSerializer):
 	owner = serializers.RelatedField(many=False, read_only=True)
+	owner_id = serializers.PrimaryKeyRelatedField(many=False, read_only=True)
+	claimed_by = serializers.RelatedField(many=False, read_only=True)
+
 	class Meta:
 		model = Task
-		fields = ('owner', 'title', 'desc', 'threshold', 'timeStamp')
+		fields = ('owner', 'owner_id', 'title', 'desc', 'threshold', 'timeStamp', 'claimed_by')
+
