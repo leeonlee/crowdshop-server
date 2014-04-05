facc97e9d8df6324c274f0b82497a58e4f0a0f8f
Leon Lee <93leonlee@gmail.com>
2014-04-05 00:58:37 -0400

    fix typos in models.py, add related names

--- crowdshop/models.py
+++ crowdshop/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Task(models.Model):
-	owner = models.ForeignKey(User)
+	owner = models.ForeignKey(User, related_name="owner")
 	title = models.CharField(max_length=255)
 	desc = models.CharField(max_length=255)
-	claimed_by = models.ForeignKey(User)
+	claimed_by = models.ForeignKey(User, related_name="claimed_by")
 	threshold = models.IntegerField()
+
+	def __unicode__(self):
+		return self.title
