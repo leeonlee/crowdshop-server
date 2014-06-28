cabddd1ab42950fbd9cde0295546814c26ecd2b9
Leon Lee <93leonlee@gmail.com>
2014-06-28 18:01:48 -0400

    move apis to /api and use index for oauth

--- crowdshop/urls.py
+++ crowdshop/urls.py
@@ -5,10 +5,11 @@ from crowdshop import views
 
 
 urlpatterns = patterns('',
-	url(r'^$', views.api_root, name='api_root'),
+	url(r'^api$', views.api_root, name='api_root'),
 	url(r'^users/$', views.UserList.as_view(), name='userlist'),
 	url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='userdetail'),
 	url(r'^tasks/$', views.TaskList.as_view(), name='tasklist'),
 	url(r'^tasks/(?P<pk>[0-9]+)/$', views.TaskDetail.as_view(), name='taskdetail'),
 	url(r'^claim/$', views.claimTask, name='claimtask'),
+	url(r'^', views.index, name='index'),
 )
