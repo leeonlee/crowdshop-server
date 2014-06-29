28e52a22b768702f0df1d9e560d692108ed84300
Leon Lee <93leonlee@gmail.com>
2014-06-29 02:41:24 -0400

    add person to admin

--- crowdshop/admin.py
+++ crowdshop/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from crowdshop.models import Task, State
+from crowdshop.models import Task, State, Person
 
 # Register your models here.
 admin.site.register(Task)
 admin.site.register(State)
+admin.site.register(Person)
