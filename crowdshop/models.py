03f1ddad5c00f21282e99721f9585ad134de0e02
Leon Lee <93leonlee@gmail.com>
2014-07-13 17:09:24 -0400

    make venmo id unique

--- crowdshop/models.py
+++ crowdshop/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class MyUser(AbstractUser):
-	venmo_id = models.CharField(max_length=30)
+	venmo_id = models.CharField(max_length=30, unique=True)
 
 class State(models.Model):
 	"""
